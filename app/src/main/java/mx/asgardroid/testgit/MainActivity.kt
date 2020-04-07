612a4877a3b856087f7d60ff54b957f7d7ffce68
Asgardroid <asgardroid@gmail.com>
2020-04-06 21:32:07 -0500

    1er Commit de Ramza

--- app/src/main/java/mx/asgardroid/testgit/MainActivity.kt
+++ app/src/main/java/mx/asgardroid/testgit/MainActivity.kt
@@ -2,11 +2,14 @@ package mx.asgardroid.testgit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("Main","Ramza Dev")
     }
 }
